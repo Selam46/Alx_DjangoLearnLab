b13abc416fe05e5dd45c0558e84ba0166530103a
selam <selamawitmesfin55@gmail.com>
2025-03-01 00:51:17 -0800

    Added book views with permission decorators and updated README.md

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Post
 from .forms import PostForm
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def books(request):
+    if request.method == 'POST':
+        # Logic for creating a new book (for example)
+        pass
+    return render(request, 'bookshelf/books.html')
 
 @permission_required('blog.can_view', raise_exception=True)
 def view_post(request, post_id):
