76285f528604875815451d6ac5f79eddf24dd684
selam <selamawitmesfin55@gmail.com>
2025-03-01 00:24:11 -0800

    deleted a folder

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Show these fields in the admin list view
-    search_fields = ('title', 'author')  # Allow searching by title and author
-    list_filter = ('publication_year',)  # Add a filter by publication year
