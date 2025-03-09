91e1a45e5f613a1d70a4b70a64fc75cabf324b54
selam <selamawitmesfin55@gmail.com>
2025-03-09 01:15:48 -0800

    Added Authentication and Permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
+from rest_framework.permissions import IsAuthenticated
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):  
@@ -8,6 +9,7 @@ class BookList(generics.ListAPIView):
     serializer_class = BookSerializer
 
 
-class BookViewSet(viewsets.ModelViewSet):  
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  
