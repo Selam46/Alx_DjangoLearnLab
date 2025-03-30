b24a0bf572c4ef1490716aee49f10073efd90537
selam <selamawitmesfin55@gmail.com>
2025-03-30 06:10:46 -0700

    Social Media API setup with authentication

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, LoginView, ProfileView
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+]
